cafb93cdd8312564cdf3addff20a3d4548a0751e
seng0226 <senghyun123@naver.com>
2024-02-26 13:33:18 +0900

    update_02_26

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const cors = require('cors')
+const path = require('path');
+const helmet = require("helmet")
+const port = 8066;
+// Use the whole root as static files to be able to serve the html file and
+// the build folder
+app.use(express.static(path.join(__dirname, '/')));
+// Send html on '/'path
+app.get('/', (req, res) => {
+res.sendFile(path.join(__dirname, + '/index.html'));
+})
+app.use(
+    helmet({
+      xFrameOptions: { action: "deny" },
+    })
+);
+
+app.use(cors());
+// Create the server and listen on port
+http.createServer(app).listen(port, () => {
+console.log(`Server running on localhost:${port}`);
+});
